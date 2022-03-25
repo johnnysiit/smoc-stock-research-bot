31ad6a41532a48d655b5fbf3ea40fe6c8b3b013f
johnnysit <37731817+johnnysiit@users.noreply.github.com>
2022-03-24 20:03:58 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+
+#Intro 
+print("\n==========================================\n|| Welcome to SMOC Data Process System ||\n==========================================\n")
+stock_ticker = str(input("Please type in the symbol of the stock: "))
