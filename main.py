d669f8113358bbd41ddbe29d3c54b01d13955aec
johnnysit <37731817+johnnysiit@users.noreply.github.com>
2022-03-24 20:40:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 
+
 #Intro 
 print("\n==========================================\n|| Welcome to SMOC Data Process System ||\n==========================================\n")
 stock_ticker = str(input("Please type in the symbol of the stock: "))
