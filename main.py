6d5cb2a2e5f5b45bed7f665dc77b16215f61ab68
johnnysit <37731817+johnnysiit@users.noreply.github.com>
2022-03-24 20:22:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-
-
+import yfinance as yf
+from yahoofinancials import YahooFinancials
 
 #Intro 
 print("\n==========================================\n|| Welcome to SMOC Data Process System ||\n==========================================\n")
 stock_ticker = str(input("Please type in the symbol of the stock: "))
+ticker = YahooFinancials(stock_ticker)
+
+#Scrap data
+ebit = ticker.get_ebit()
+print (ebit)
