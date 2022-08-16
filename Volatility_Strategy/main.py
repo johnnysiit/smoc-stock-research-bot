45ac58020b281126139c2b605b85ef9e8cf61237
johnnysit <37731817+johnnysiit@users.noreply.github.com>
2022-08-16 18:21:24 -0500

    Updated on VOI

--- Volatility_Strategy/main.py
+++ Volatility_Strategy/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 import os
 import time
-import option_price as op
+import Volatility_Strategy.option_price as op
 
 def get_finviz_data(ticker):
     stock = finvizfinance(ticker)
@@ -16,9 +16,22 @@ def percentage_process(number):
     number = abs(number)
     return number/100
 
+def clean_sheet(dic):
+    new_dict = dict()
+    new_dict["Price"] = dic["Price"]
+    new_dict["Earnings"] = dic["Earnings"]
+    new_dict["Total Volatility"] = str((dic["Total Volatility"]*100))+"%"
+    new_dict["Option Total"] = dic["Option Total"]
+    new_dict["Option to Price"] = str((dic["Option to Price"]*100))+"%"
+    new_dict["JVOI"] = dic["JVOI"] = str((dic["JVOI"]*100))+"%"
+    return new_dict
+
 def main():
-    ticker_list = ["LOW","ADI","TJX","TGT","SNPS","NIO","KEYS","ZTO","EC","AMCR","EBR","ELP","WOLF","PFGC","BBWI","ZIM","BAK","WB","EVO","CIG","SID","SRAD","DNUT","YY","CINT","SFL","MCG","EXAI","TUYA","MAG","HYZN","NNDM","JBSS","TGS","CRMT","NESR","PLCE","TUFN","RADA","AURC","ORMP","GRIN","ZOM","KMDA","IREN","RAAS","SMED","MDXH","LUCD","BEST","OTMO","AIH","CRWS","CIH","VTVT","BDL","TPHS","DGLY","MXC","DGHI","AIKI","BSGM","NAVB","ALRN","ELTK","BTBD","HOTH","MOBQ","NEPT","RMED"]
-    # ticker_list = ["LOW"]
+    file = open("./Volatility_Strategy/ticker.txt", "r")
+    ticker = file.readlines()
+    ticker_list = []
+    for k in ticker:
+        ticker_list.append(k.strip())
     final_data = dict()
     count = 0
     for i in ticker_list:
@@ -27,16 +40,21 @@ def main():
             print ("8 secs cooldown")
             time.sleep(8)
         try:
-            stock_data = get_finviz_data(i)
+            try:
+                stock_data = get_finviz_data(i)
+            except:
+                print (i, "... Finviz Finance Fetching Failure")
             filter_data = dict()
             if stock_data["Optionable"] == "Yes":
                 for j in stock_data:
                     if j == "Price":
                         filter_data["Price"] = float(stock_data[j])
+                    elif j == "Earnings":
+                        filter_data["Earnings"] = stock_data[j]
                     elif j == "Perf Week":
                         filter_data["Perf Week"] = percentage_process(stock_data[j])
-                    elif j == "Perf Quarter":
-                        filter_data["Perf Quarter"] = percentage_process(stock_data[j])
+                    elif j == "Perf Month":
+                        filter_data["Perf Month"] = percentage_process(stock_data[j])
                     elif j == "52W Low":
                         filter_data["52W Low"] = percentage_process(stock_data[j])
                     elif j == "52W High":
@@ -47,7 +65,7 @@ def main():
                         filter_data["Volatility M"] = percentage_process(stock_data[j])
                     elif j == "Change":
                         filter_data["Change"] = percentage_process(stock_data[j])
-                filter_data["Total Volatility"] = (filter_data["Perf Week"]*5 + filter_data["Perf Quarter"] + filter_data["52W Low"] + filter_data["52W High"] + filter_data["Volatility W"]*5 + filter_data["Volatility M"]*2 + filter_data["Change"]*10)/5
+                filter_data["Total Volatility"] = (filter_data["Perf Week"]*5*6 + filter_data["Perf Month"]*6 + filter_data["52W Low"] + filter_data["52W High"] + filter_data["Volatility W"]*5*6*2 + filter_data["Volatility M"]*6*2 + filter_data["Change"]*180)/(180*9)
                 option = op.OptionPrice(i,filter_data["Price"])
                 opc = option.get_call()
                 opp = option.get_put()
@@ -55,10 +73,15 @@ def main():
                 filter_data["Put"] = opp
                 total = opc + opp
                 filter_data["Option Total"] = total
-                final_data[i] = filter_data
-                print (i,"...Fetched Successfully")
+                filter_data["Option to Price"] = total/filter_data["Price"]
+                filter_data["JVOI"] = filter_data["Total Volatility"]/filter_data["Option to Price"]
+                cleaner_sheet = clean_sheet(filter_data)
+                final_data[i] = cleaner_sheet
+                print (i,"... Fetched Successfully")
+            else:
+                print (i,"... Not Optionable")
         except:
-            print (i,"...Failed")
+            print (i,"... Yahoo Finance Fetching Failure")
             continue
 
     df = pd.DataFrame(final_data)
@@ -69,5 +92,3 @@ def main():
         print ("\n数据输出成功于Output文件夹\nData output successfully in Output folder\n")
     except:
         print ("\n数据输出失败 请检查输出权限\nFailed to output data, please check output permission")
-
-main()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import mgs
 import sct
 import fin_stmt_excel as fse
+import Volatility_Strategy.main as vs
+
 
 def get_tickers():
     ticker = str(input("请输入股票代码 Please type in the symbol of the stock: "))
@@ -9,21 +11,31 @@ def get_tickers():
 
 print("\n==========================================\n|| Welcome to SMOC Data Process System ||\n==========================================\n")
 print("请选择运行程序 Pleae select the program you want to run:")
-print("1. Stock Comparison Tool\n2. Financial Statement\n3. SMOC Matrix Grading System")
+print("1. Stock Comparison Tool\n2. Financial Statement Grading System\n3. Volatility Strategy\n4. Exit")
 program_run = input("Please enter a number: ")
 
 if program_run == "1":
     sct.main()
 elif program_run == "2":
-    ticker,output_mode = get_tickers()
-    print("\n请稍等，总时长可能会超过2分钟 Please wait... The whole process might take over 2 minutes....\n")
-    fse.main(ticker,output_mode)
+    mode = input("\nPlease select the mode you want to run: \n1. Multi-stock Metrix Ranking\n2. Single Stock Metrix Detail Analysis\n3. Get an Excel financial statement only\nPlease enter a number: ")
+    if mode == "3":
+        ticker,output_mode = get_tickers()
+        print("\n请稍等，总时长可能会超过2分钟 Please wait... The whole process might take over 2 minutes....\n")
+        fse.main(ticker,output_mode)
+    elif mode == "2":
+        print("\nAttention: This system cannot apply on the data of financial industry.\n请注意：该系统不适用于金融行业的财报数据\n")
+        ticker,output_mode = get_tickers()
+        runmode = int(input("\n1.Normal Mode\n2.Fast Mode (Skip Preferred Stock Dividend)\n请选择运行模式 Please select the run mode: "))
+        print("\nNow analyzing the data\n开始分析数据\n")
+        mgs.main(ticker,output_mode,runmode)
+        print("\nFRAPS Finished Running!\nFARPS完成运行，感谢支持!\n")
+    elif mode == "1":
+        print ("This function is not ready yet. If you want to try this function, run ranking.py instead.")
 elif program_run == "3":
-    print("\nAttention: This system cannot apply on the data of financial industry.\n请注意：该系统不适用于金融行业的财报数据\n")
-    ticker,output_mode = get_tickers()
-    runmode = int(input("\n1.Normal Mode\n2.Fast Mode (Skip Preferred Stock Dividend)\n请选择运行模式 Please select the run mode: "))
-    print("\nNow analyzing the data\n开始分析数据\n")
-    mgs.main(ticker,output_mode,runmode)
-    print("\nFRAPS Finished Running!\nFARPS完成运行，感谢支持!\n")
+    notice = input("\nPlease enter your stock list on the file /Volatility_Strategy/ticker.txt\nWhen you ready, press enter to continue")
+    vs.main()
+elif program_run == "4":
+    print("\nThank you for using this program!\n感谢使用！\n")
+    exit()
 else:
     print ("\nPlease enter a valid number!\n请输入有效数字！\n")
