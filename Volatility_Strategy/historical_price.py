eb117ad00480b2a9d81d4edd7dc4eb1b0aca47c2
johnnysit <37731817+johnnysiit@users.noreply.github.com>
2022-08-20 11:03:53 -0500

    Bug fix

---
+++ Volatility_Strategy/historical_price.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+
+def get_cv(ticker):
+    yfinance = yf.Ticker(ticker)
+    hist = yfinance.history(period="6mo")
+    price_list = []
+    for i in hist["Open"]:
+        price_list.append(float(i))
+    for k in hist["Close"]:
+        price_list.append(float(k))
+    for j in hist["High"]:
+        price_list.append(float(j))
+    for l in hist["Low"]:
+        price_list.append(float(l))
+    print (price_list)
+    #get mean on price list
+    mean = sum(price_list)/len(price_list)
+    # get standard deviation on price list
+    std = sum([(i-mean)**2 for i in price_list])/len(price_list)
+    std = std**0.5
+    return std/mean
+print (get_cv("CRMT"))
+
--- Volatility_Strategy/main.py
+++ Volatility_Strategy/main.py
@@ -4,6 +4,7 @@ import datetime
 import os
 import time
 import Volatility_Strategy.option_price as op
+import yfinance as yf
 
 def get_finviz_data(ticker):
     stock = finvizfinance(ticker)
@@ -21,10 +22,31 @@ def clean_sheet(dic):
     new_dict["Price"] = dic["Price"]
     new_dict["Earnings"] = dic["Earnings"]
     new_dict["Total Volatility"] = str((dic["Total Volatility"]*100))+"%"
+    new_dict["CV"] = str((dic["CV"]*100))+"%"
     new_dict["Option Total"] = dic["Option Total"]
     new_dict["Option to Price"] = str((dic["Option to Price"]*100))+"%"
-    new_dict["JVOI"] = dic["JVOI"] = str((dic["JVOI"]*100))+"%"
+    new_dict["JVOI"] = str((dic["JVOI"]*100))+"%"
+    new_dict["JVOI2"] = str((dic["JVOI2"]*100))+"%"
     return new_dict
+
+def get_cv(ticker):
+    yfinance = yf.Ticker(ticker)
+    hist = yfinance.history(period="6mo")
+    price_list = []
+    for t in hist["Open"]:
+        price_list.append(float(t))
+    for k in hist["Close"]:
+        price_list.append(float(k))
+    for j in hist["High"]:
+        price_list.append(float(j))
+    for l in hist["Low"]:
+        price_list.append(float(l))
+    #get mean on price list
+    mean = sum(price_list)/len(price_list)
+    # get standard deviation on price list
+    std = sum([(i-mean)**2 for i in price_list])/len(price_list)
+    std = std**0.5
+    return std/mean
 
 def main():
     file = open("./Volatility_Strategy/ticker.txt", "r")
@@ -39,13 +61,16 @@ def main():
         if count%20 == 0:
             print ("8 secs cooldown")
             time.sleep(8)
+        #Get data from finviz
         try:
-            try:
-                stock_data = get_finviz_data(i)
-            except:
-                print (i, "... Finviz Finance Fetching Failure")
-            filter_data = dict()
-            if stock_data["Optionable"] == "Yes":
+            stock_data = get_finviz_data(i)
+        except:
+            print (i, "... Finviz Finance Fetching Failure")
+            continue
+        #Data cleaning and processing start
+        filter_data = dict()
+        if stock_data["Optionable"] == "Yes":
+            try: 
                 for j in stock_data:
                     if j == "Price":
                         filter_data["Price"] = float(stock_data[j])
@@ -65,7 +90,19 @@ def main():
                         filter_data["Volatility M"] = percentage_process(stock_data[j])
                     elif j == "Change":
                         filter_data["Change"] = percentage_process(stock_data[j])
-                filter_data["Total Volatility"] = (filter_data["Perf Week"]*5*6 + filter_data["Perf Month"]*6 + filter_data["52W Low"] + filter_data["52W High"] + filter_data["Volatility W"]*5*6*2 + filter_data["Volatility M"]*6*2 + filter_data["Change"]*180)/(180*9)
+                    elif j == "SMA20":
+                        filter_data["SMA20"] = percentage_process(stock_data[j])
+                    elif j == "SMA50":
+                        filter_data["SMA50"] = percentage_process(stock_data[j])
+                    elif j == "SMA200":
+                        filter_data["SMA200"] = percentage_process(stock_data[j])
+
+                filter_data["Total Volatility"] = (filter_data["Perf Week"]*3*2 + filter_data["Perf Month"]*2 + filter_data["52W Low"] + filter_data["52W High"] + filter_data["Volatility W"]*3*2 + filter_data["Volatility M"]*2 + filter_data["Change"]*5*2 + filter_data["SMA20"]*2 + filter_data["SMA50"])/(15)
+            except:
+                print (i, "... Data Cleaning Failure")
+                continue
+            #=====Get Option Price Start=====
+            try:
                 option = op.OptionPrice(i,filter_data["Price"])
                 opc = option.get_call()
                 opp = option.get_put()
@@ -73,21 +110,31 @@ def main():
                 filter_data["Put"] = opp
                 total = opc + opp
                 filter_data["Option Total"] = total
-                filter_data["Option to Price"] = total/filter_data["Price"]
+
+                filter_data["Option to Price"] = float(total/filter_data["Price"])
                 filter_data["JVOI"] = filter_data["Total Volatility"]/filter_data["Option to Price"]
+            except:
+                print (i, "... Option Price Fetching Failure")
+                continue
+            #=====Get Option Price End=====
+            try: 
+                filter_data["CV"] = float(get_cv(i))
+                filter_data["JVOI2"] = filter_data["CV"]/filter_data["Option to Price"]
                 cleaner_sheet = clean_sheet(filter_data)
                 final_data[i] = cleaner_sheet
-                print (i,"... Fetched Successfully")
-            else:
-                print (i,"... Not Optionable")
-        except:
-            print (i,"... Yahoo Finance Fetching Failure")
+            except:
+                print (i, "... Historical Price Fetching Failure")
+                continue
+            print (i,"... Fetched Successfully")
+
+        else:
+            print (i,"... Not Optionable")
             continue
 
     df = pd.DataFrame(final_data)
     current_time = datetime.datetime.now().strftime('%m-%d-%Y-%H-%M-%S')
     try:
-        current_path = os.path.dirname(os.path.abspath(__file__))
+        #current_path = os.path.dirname(os.path.abspath(__file__))
         df.to_excel("./Output/Volatility_Strategy"+current_time+".xlsx")
         print ("\n数据输出成功于Output文件夹\nData output successfully in Output folder\n")
     except:
